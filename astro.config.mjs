18d0fc3e52ef2e2231f4bbd2e7ae0d60738ca0e3
BreakingLead <breakingleadqwq@gmail.com>
2024-07-05 19:15:05 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://breakinglead.github.io',
-	base: '/blog'
+	base: '/blog',
 	integrations: [mdx(), sitemap()],
 });
