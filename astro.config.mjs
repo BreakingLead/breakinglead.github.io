30f8dad13f87b4fa6994663a9b2298751738c1a9
BreakingLead <breakingleadqwq@gmail.com>
2024-07-06 14:04:12 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://breakinglead.github.io',
-	base: '/',
-	integrations: [mdx(), sitemap()],
+  site: 'https://breakinglead.github.io',
+  base: '/',
+  integrations: [mdx(), sitemap()]
 });
