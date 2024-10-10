80ce0fbcaeb5d616c2bb0e62c1a64868a31e52fa
BreakingLead <breakingleadqwq@gmail.com>
2024-10-10 08:49:16 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import typst from 'astro-typst';
 import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
